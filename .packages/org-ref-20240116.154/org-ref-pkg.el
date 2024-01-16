9d9bae3a5365d228fbc323b64f3b97bebfe1eb71
Jordan Herzstein <jordanherzstein@gmail.com>
2024-01-15 22:42:50 -0500

    auto updating site

---
+++ .packages/org-ref-20240116.154/org-ref-pkg.el
@@ -0,0 +1,24 @@
+(define-package "org-ref" "20240116.154" "citations, cross-references and bibliographies in org-mode"
+  '((org "9.4")
+    (dash "0")
+    (s "0")
+    (f "0")
+    (htmlize "0")
+    (hydra "0")
+    (avy "0")
+    (parsebib "0")
+    (bibtex-completion "0")
+    (citeproc "0")
+    (ox-pandoc "0"))
+  :commit "6fc62e68dfa608620ae067a70db6ab31c237ad93" :authors
+  '(("John Kitchin" . "jkitchin@andrew.cmu.edu"))
+  :maintainers
+  '(("John Kitchin" . "jkitchin@andrew.cmu.edu"))
+  :maintainer
+  '("John Kitchin" . "jkitchin@andrew.cmu.edu")
+  :keywords
+  '("org-mode" "cite" "ref" "label")
+  :url "https://github.com/jkitchin/org-ref")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
